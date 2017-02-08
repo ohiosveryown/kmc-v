f04c137a38f5f1c5967a21fc65cb6db2b1a17b08
ohiosveryown <matt@ohiosveryown.co>
2017-02-08 14:04:58 -0500

    Router Active Link

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <router-link to="/"><a>Home</a></router-link>
-    <router-link to="/blog"><a>Blog</a></router-link>
+    <router-link to="/" exact><a>Home</a></router-link>
+    <router-link to="/blog" exact><a>Blog</a></router-link>
     <router-view class="view-container"></router-view>
   </div>
 </template>
