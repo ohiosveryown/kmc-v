9885110a75128d3f9b3f24513ca21bfa108280e7
ohiosveryown <matt@ohiosveryown.co>
2017-02-10 22:08:56 -0500

    Init JS toggle

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -57,8 +57,11 @@
               <p>Lorem ipsum dolor sit amet, mel at vocent docendi, ne sed tale theophrastus conclusionemque. Vix sententiae repudiandae an, no usu epicuri constituto, sed facilisi argumentum id. Nam prima petentium mnesarchum ex.</p>
               <p>Augue sonet et vel, posse affert efficiantur usu ut. Eu eam quem nonumy democritum, liber putent legendos eu ius.</p>
               <p>Ut nam civibus indoctum. Eum ne atqui primis. Quo no nulla quaestio. An ludus putant sea, id postea antiopam mea, ut omnis everti expetenda sit. Inani errem expetenda per ad, te adipisci perpetua per, nulla noluisse recteque quo id.</p>
-              <p>Id usu scaevola assueverit temporibus, regione denique an nec, alia delenit laoreet vel et.</p>
-              <button v-on:click="showAlert">Try it</button>
+              <p>Id usu scaevola assueverit temporibus, regione denique an nec, alia delenit laoreet vel etttttt.</p>
+              <div onclick="toggleClass(this)" class="toggle">
+                <div class="ball"></div>
+                <div class="track"></div>
+              </div>
             </div>
           </div>
         </article>
@@ -94,22 +97,47 @@
 
   section { min-height: 300vh; }
 
+  .ball {
+    background: #f7f7f7;
+    border: 1px solid #9fb0b7;
+    border-radius: 100px;
+    height: 32px; width: 32px;
+    transform: translate3d(0,0,0);
+    transition: all 300ms cubic-bezier(0.680, -0.6, 0.265, 1.8);
+  }
+
+  .track {
+    background: #9fb0b7;
+    border-radius: 100px;
+    height: 20px;
+    width: 56px;
+    position: relative;
+    top: -26px;
+    left: 4px;
+    z-index: -1;
+    box-shadow: none;
+    transition: all 250ms ease;
+    transition-delay: 100ms;
+    box-shadow: inset 0 0 0 0 #49bde4;
+  }
+
+  .move {
+    .ball {
+      transform: translate3d(30px,0,0);
+    }
+
+    .track {
+      box-shadow: inset 0 0 0 10px #49bde4;
+    }
+  }
+
 </style>
 
 
 <!-- script -->
 <script>
-export default {
-  data () {
-    return {
 
-      msg: 'Hello World!'
-    }
-  },
-  methods: {
-    showAlert: () => {
-      alert('test')
-    }
+  toggleClass = function(e) {
+    (e).classList.toggle('move');
   }
-}
 </script>
