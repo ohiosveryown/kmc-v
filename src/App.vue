84eb2e02108027683251e57adb6910c46645b9f0
ohiosveryown <matt@ohiosveryown.co>
2017-02-20 11:43:34 -0500

    Routes / Nav

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <!-- template -->
 <template>
   <div id="app">
+    <!-- routes / nav -->
     <router-link to="/" exact><a>Home</a></router-link>
     <router-link to="/blog" exact><a>Blog</a></router-link>
+    <!-- view -->
     <router-view class="view-container"></router-view>
   </div>
 </template>
